1ffadcf18c8db37c5efc733949a8439ba45473c0
Skander Mzali <skanderm@gmail.com>
2023-08-08 14:48:41 -0700

    Ensure Gettext is compiled

--- lib/ash_admin/gettext.ex
+++ lib/ash_admin/gettext.ex
@@ -1,24 +1,26 @@
-defmodule AshAdmin.Gettext do
-  @moduledoc """
-  A module providing Internationalization with a gettext-based API.
+if Code.ensure_compiled!(Gettext) do
+  defmodule AshAdmin.Gettext do
+    @moduledoc """
+    A module providing Internationalization with a gettext-based API.
 
-  By using [Gettext](https://hexdocs.pm/gettext),
-  your module gains a set of macros for translations, for example:
+    By using [Gettext](https://hexdocs.pm/gettext),
+    your module gains a set of macros for translations, for example:
 
-      import AshAdmin.Gettext
+        import AshAdmin.Gettext
 
-      # Simple translation
-      gettext("Here is the string to translate")
+        # Simple translation
+        gettext("Here is the string to translate")
 
-      # Plural translation
-      ngettext("Here is the string to translate",
-               "Here are the strings to translate",
-               3)
+        # Plural translation
+        ngettext("Here is the string to translate",
+                "Here are the strings to translate",
+                3)
 
-      # Domain-based translation
-      dgettext("errors", "Here is the error message to translate")
+        # Domain-based translation
+        dgettext("errors", "Here is the error message to translate")
 
-  See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
-  """
-  use Gettext, otp_app: :ash_admin
+    See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
+    """
+      use Gettext, otp_app: :ash_admin
+  end
 end
